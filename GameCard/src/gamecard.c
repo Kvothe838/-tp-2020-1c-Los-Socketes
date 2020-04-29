8b2cc548e933024cf787414ccfb11a8896b7d03c
kvothe838 <facundoyuffrida@gmail.com>
2020-04-28 23:39:57 -0300

    Intento de fix de todo el maldito proyecto.

--- GameCard/src/gamecard.c
+++ GameCard/src/gamecard.c
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 
 int main(void) {
-	int prueba = crear_conexion("189", "189");
+	int prueba = crear_conexion("127.0.0.1", "4444");
 	t_log* logger = log_create("testGameCard.log", "GAMECARD", true, LOG_LEVEL_TRACE);
 	log_trace(logger, "Conexión dio %d", prueba);
+	log_destroy(logger);
 	return EXIT_SUCCESS;
 }
 
