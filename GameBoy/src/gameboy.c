cb6367a8142a914da9161b6aa6b0354ce85d61aa
carlosnikc <alexis_dad@hotmail.com>
2020-05-25 18:48:16 -0300

    WIP agrego mensaje para TEAM de APPEARED

--- GameBoy/src/gameboy.c
+++ GameBoy/src/gameboy.c
@@ -47,35 +47,46 @@ if (strcmp(argv[1],"BROKER") == 0){
 					conexionBroker = crear_conexion_cliente(ipBroker, puertoBroker);
 
 					if (strcmp(argv[2],"NEW_POKEMON") == 0){
-						NewPokemon pokemon;
-						pokemon.nombre = argv[3];
-						pokemon.largoNombre = strlen(pokemon.nombre);
-						pokemon.posX = atoi(argv[4]);
-						pokemon.posY = atoi(argv[5]);
-						pokemon.cantidad = atoi(argv[6]);
+						//t_persona *ptr_persona = (t_persona*) malloc(sizeof(t_persona)); 
+						//(*ptr_persona).dni = 41543667;
+						//ptr_persona -> dni = 41543667;
+						//free(ptr_persona);
+						// Si se tuviese la estructura dinámica ejemplificada anteriormente, se deberá hacer lo mismo con los punteros "internos"
+						//free((*ptr_paquete).username);
+						//free((*ptr_paquete).message);
+						//free(ptr_paquete);
+
+						NewPokemon *pokemon = (NewPokemon*) malloc(sizeof(NewPokemon));
+						pokemon->nombre = argv[3];
+						pokemon->largoNombre = strlen(pokemon->nombre);
+						pokemon->posX = atoi(argv[4]);
+						pokemon->posY = atoi(argv[5]);
+						pokemon->cantidad = atoi(argv[6]);
 						
 						Buffer* buffer = malloc(sizeof(Buffer));
 
-						buffer->size = sizeof(uint32_t) * 4 + strlen(pokemon.nombre) + 1;
+						buffer->size = sizeof(uint32_t) * 4 + strlen(pokemon->nombre) + 1;
 
 						void* stream = malloc(buffer->size);
 						int offset = 0;
 
-						memcpy(stream + offset, &pokemon.posX, sizeof(uint32_t));
+						memcpy(stream + offset, &pokemon->posX, sizeof(uint32_t));
 						offset += sizeof(uint32_t);
 
-						memcpy(stream + offset, &pokemon.posY, sizeof(uint32_t));
+						memcpy(stream + offset, &pokemon->posY, sizeof(uint32_t));
 						offset += sizeof(uint32_t);
 
-						memcpy(stream + offset, &pokemon.cantidad, sizeof(uint32_t));
+						memcpy(stream + offset, &pokemon->cantidad, sizeof(uint32_t));
 						offset += sizeof(uint32_t);
 
-						memcpy(stream + offset, &pokemon.largoNombre, sizeof(uint32_t));
+						memcpy(stream + offset, &pokemon->largoNombre, sizeof(uint32_t));
 						offset += sizeof(uint32_t);
 
-						memcpy(stream + offset, pokemon.nombre, strlen(pokemon.nombre) + 1);
+						memcpy(stream + offset, pokemon->nombre, strlen(pokemon->nombre) + 1);
 
 						buffer->stream = stream;
+
+						free(pokemon);
 
 						//Entonces podemos llenar el paquete con el buffer///
 
@@ -114,6 +125,65 @@ else if (strcmp(argv[1],"TEAM") == 0){
 						puertoTeam = config_get_string_value(config, "PUERTO_TEAM");
 						log_info(logger, "IP_TEAM     %s y PUERTO_TEAM     %s", ipTeam, puertoTeam);
 						conexionTeam = crear_conexion_cliente(ipTeam, puertoTeam);
+
+						AppearedPokemon *pokemon = (AppearedPokemon*) malloc(sizeof(AppearedPokemon));
+						pokemon->nombre = argv[3];
+						pokemon->largoNombre = strlen(pokemon->nombre);
+						pokemon->posX = atoi(argv[4]);
+						pokemon->posY = atoi(argv[5]);
+						
+						Buffer* buffer = malloc(sizeof(Buffer));
+
+						buffer->size = sizeof(uint32_t) * 3 + strlen(pokemon->nombre) + 1;
+
+						void* stream = malloc(buffer->size);
+						int offset = 0;
+
+						memcpy(stream + offset, &pokemon->posX, sizeof(uint32_t));
+						offset += sizeof(uint32_t);
+
+						memcpy(stream + offset, &pokemon->posY, sizeof(uint32_t));
+						offset += sizeof(uint32_t);
+
+						memcpy(stream + offset, &pokemon->largoNombre, sizeof(uint32_t));
+						offset += sizeof(uint32_t);
+
+						memcpy(stream + offset, pokemon->nombre, strlen(pokemon->nombre) + 1);
+
+						buffer->stream = stream;
+
+						free(pokemon);
+
+						//Entonces podemos llenar el paquete con el buffer///
+
+						Paquete* paquete = malloc(sizeof(paquete));
+
+						paquete->codigoOperacion = APPEARED;
+						paquete->buffer = buffer;
+
+						//Armamos el stream a enviar//
+						void* aEnviar = malloc(buffer->size + sizeof(TipoCola) + sizeof(uint32_t));
+						int offsetS = 0;
+
+						memcpy(aEnviar + offsetS, &(paquete->codigoOperacion), sizeof(TipoCola));
+						offsetS += sizeof(TipoCola);
+
+						memcpy(aEnviar + offsetS, &(paquete->buffer->size), sizeof(uint32_t));
+						offsetS += sizeof(uint32_t);
+
+						memcpy(aEnviar + offsetS, paquete->buffer->stream, paquete->buffer->size);
+
+						//Enviamos//
+						send(conexionTeam, aEnviar, buffer->size + sizeof(TipoCola) + sizeof(uint32_t), 0);
+
+						log_info(logger, "El mensaje enviado es: %s %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4], argv[5]);
+						
+						liberar_conexion_cliente(conexionTeam);
+
+						free(aEnviar);
+						free(paquete->buffer->stream);
+						free(paquete->buffer);
+						free(paquete);
 }
 else if (strcmp(argv[1],"GAMECARD") == 0){
 						ipGamecard = config_get_string_value(config, "IP_GAMECARD");
