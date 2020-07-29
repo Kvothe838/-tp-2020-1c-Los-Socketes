d2a2f9deae4c25ed267193e2e111204cd9e1a8ce
ariel-sng <arielsinge@hotmail.com>
2020-07-29 16:54:38 -0300

    suscripción a broker hecho, y recibimiento correcto de las colas NEW, GET & CATCH

--- GameCard/src/gamecard.c
+++ GameCard/src/gamecard.c
@@ -10,13 +10,23 @@
 int main(void) {
 	t_log* logger = iniciar_logger("gamecard.log", "GAMECARD");
 	t_config* config = leer_config("GameCard.config", logger);
-	char *ip, *puerto;
 
 	inicializarData(logger);
 
 	log_info(logger, "ID del proceso GameCard: %d", process_getpid());
 
-	//iniciar_servidor("127.0.0.1", "5001");
+
+
+	char* ipBroker, *puertoBroker;
+	ipBroker = config_get_string_value(config, "IP_BROKER");
+	puertoBroker = config_get_string_value(config, "PUERTO_BROKER");
+
+	int conexionBroker = crear_conexion_cliente(ipBroker, puertoBroker);
+
+	int suscripcionEnviada = enviarSuscripcion(conexionBroker, GAMECARD, 3, NEW, GET, CATCH);
+
+	if(suscripcionEnviada)
+		iniciar_servidor(config, conexionBroker);
 	
 	/*inicializarData(logger);
 
@@ -57,7 +67,7 @@ int main(void) {
 	//Para pruebas de suscripción con Broker. Comentar si no se usa pero no borrar plis.
 
 
-	ip = config_get_string_value(config, "IP_BROKER");
+	/*ip = config_get_string_value(config, "IP_BROKER");
 	puerto = config_get_string_value(config, "PUERTO_BROKER");
 	int socketCliente = crear_conexion_cliente(ip, puerto);
 	enviarSuscripcion(socketCliente, GAMECARD, 3, NEW, GET, CAUGHT);
@@ -74,7 +84,7 @@ int main(void) {
 				free(mensaje);
 			}
 		}
-	}
+	}*/
 
 	//liberarVariablesGlobales();
 
@@ -83,12 +93,3 @@ int main(void) {
 
 	return EXIT_SUCCESS;
 }
-
-
-
-
-
-
-
-
-
