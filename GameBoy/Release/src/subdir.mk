fe0d66ae17d2a1938acca2c18cbe0f765872dd15
ariel-sng <arielsinge@hotmail.com>
2020-08-06 03:57:30 -0300

    Cambios para Broker en consola

--- GameBoy/Release/src/subdir.mk
+++ GameBoy/Release/src/subdir.mk
@@ -4,26 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/broker.c \
-../src/brokerColas.c \
-../src/conexionBroker.c 
+../src/gameboy.c 
 
 OBJS += \
-./src/broker.o \
-./src/brokerColas.o \
-./src/conexionBroker.o 
+./src/gameboy.o 
 
 C_DEPS += \
-./src/broker.d \
-./src/brokerColas.d \
-./src/conexionBroker.d 
+./src/gameboy.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/tp-2020-1c-Los-Socketes/Shared" -I"/home/utnso/tp-2020-1c-Los-Socketes/Shared/Debug" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
