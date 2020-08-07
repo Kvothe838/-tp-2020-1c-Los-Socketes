c8f31dc4b2c89d47030e2b93ca6605858cdecaf9
Facundo <facundo@omniasalud.com>
2020-08-07 18:08:32 -0300

    Logs y más logs.

--- Shared/shared/connection.c
+++ Shared/shared/connection.c
@@ -2,23 +2,32 @@
 
 int crear_conexion_cliente(char *ip, char* puerto)
 {
+  printf("8.1");
 	struct addrinfo hints;
 	struct addrinfo *server_info;
 
 	memset(&hints, 0, sizeof(hints));
+  printf("8.2");
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
 	getaddrinfo(ip, puerto, &hints, &server_info);
+  printf("8.3");
 
 	int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+  printf("8.4: %d", socket_cliente);
 
 	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1){
 		socket_cliente = -1;
 	}
 
+  printf("8.5");
+
 	freeaddrinfo(server_info);
+
+  printf("8.6");
 
 	return socket_cliente;
 }
